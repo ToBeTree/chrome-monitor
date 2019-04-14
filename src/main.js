b30f77cf07859ac45dc1cb09195e87ae92436b7c
wuyongqiang <wyqexpect@163.com>
2019-04-14 23:11:38 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -6,3 +6,28 @@ const program = require('commander')
 
 program.option('-p,--platform [type]', 'runtime env').parse(process.argv)
 if (program.platform) console.log(program.platform)
+
+const data = {
+    platform: '', //运行平台
+    company: '', //公司
+    url: '', //页面地址
+    repo: '', //仓库地址 可选参数
+    branch: '', //分支名 可选参数
+    batch: '', //请求批次
+    pmo: '', //pmo号 可选参数
+    pageType: '', //页面地址类型
+    requestCount: 0, //页面中请求总数
+    startTime: '', //开始请求时间
+    endTime: '', //结束请求时间
+    redirect: 0, //页面重定向耗时
+    dns: 0, //DNS查找耗时
+    tcp: 0, //TCP链接耗时
+    request: 0, //请求发送耗时
+    response: 0, //响应接收耗时
+    domReady: 0, //DOMReady耗时
+    load: 0, //页面加载耗时
+    downloadSize: 0, //页面下载总量
+    captrueShot: '' //页面快照
+}
+
+const type = []
